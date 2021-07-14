3ccd9b0793bcae9e64fc6d32a78293989389e688
Tomas Padilla <padillatomasagustin@gmail.com>
2021-07-14 11:05:27 +0200

    Loading Design System

---
+++ src/infrastructure/theme/colors.js
@@ -0,0 +1,34 @@
+export const COLORS = {
+  AppBGs: {
+    WhiteBG: "#ffffff",
+    InputDefaultBG: "#f2f2f2",
+    InputPressedBG: "#e2e2e2",
+    ModalBG: rgba(0, 0, 0, 0.25),
+  },
+  Texts: {
+    WhiteTXT: "#ffffff",
+    BlackTXT: "#17171b",
+    GreyTXT: "#747476",
+    NumberTXT: rgba(23, 23, 27, 0.6),
+  },
+  PokeTypes: {
+    bug: "#8cb230",
+  },
+  PokeTypeBGs: {
+    bugBG: "#8bd674",
+  },
+  PokeHeights: {
+    ShortH: "#ffc5e6",
+    MediumH: "#aebfd7",
+    TallH: "#aaacb8",
+  },
+  PokeWeights: {
+    LightW: "#99cd7c",
+    NormalW: "#57b2dc",
+    HeavyW: "#5a92a5",
+  },
+  ButtonBG: {
+    PrimaryBG: "#ea5d60",
+    SecondaryBG: "#f2f2f2",
+  },
+};
